d3509acf788635728d68e4186f9d524577c97272
lunar-origin <43428355+lunar-origin@users.noreply.github.com>
2021-06-21 12:23:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -163,6 +163,41 @@ this.options.datacommit = false
           "type": "lab.html.Page",
           "items": [
             {
+              "required": true,
+              "type": "radio",
+              "label": "性別についてあてはまるものをクリックしてください。",
+              "options": [
+                {
+                  "label": "男性",
+                  "coding": "3"
+                },
+                {
+                  "label": "女性",
+                  "coding": "2"
+                },
+                {
+                  "label": "どちらでもない",
+                  "coding": "1"
+                }
+              ],
+              "name": ""
+            }
+          ],
+          "scrollTop": true,
+          "submitButtonText": "次へ →",
+          "submitButtonPosition": "right",
+          "files": {},
+          "responses": {
+            "": ""
+          },
+          "parameters": {},
+          "messageHandlers": {},
+          "title": "gender"
+        },
+        {
+          "type": "lab.html.Page",
+          "items": [
+            {
               "type": "text",
               "title": "はじめに",
               "content": "はじめに,  次の画面に表示される短い記事を読んで頂きます。\n実験の最後に, 記事の内容について簡単なアンケートを実施しますので, 書かれている内容をイメージしながらよく読んでください。（「次へ」ボタン）"
